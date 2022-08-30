a1678237bd37e90e578f0017b82552d99f7de18a
Aleksei Chikin <mail@makingoff.name>
2022-08-30 15:40:56 +0300

    Refactors button control css definition

--- controls/button.js
+++ controls/button.js
@@ -2,6 +2,14 @@ import ControlControl from './control'
 import getIcon from '../utils/get-icon'
 
 export default class ControlButton extends ControlControl {
+	get css() {
+		return {
+			container: 'contenteditor__control',
+			selected: 'contenteditor__control--selected',
+			disabled: 'contenteditor__control--disabled'
+		}
+	}
+
 	handleAction(event) {
 		this.handler(this.params.action, event)
 	}
@@ -27,15 +35,15 @@ export default class ControlButton extends ControlControl {
 
 	getElement(node) {
 		const classNames = [
-			'contenteditor__control'
+			this.css.container
 		]
 
 		if (this.params.selected && (typeof this.params.selected === 'function' && this.params.selected(node) || this.params.selected === true)) {
-			classNames.push('contenteditor__control--selected')
+			classNames.push(this.css.selected)
 		}
 
 		if (this.params.disabled && (typeof this.params.disabled === 'function' && this.params.disabled(node) || this.params.disabled === true)) {
-			classNames.push('contenteditor__control--disabled')
+			classNames.push(this.css.disabled)
 		}
 
 		this.element.className = classNames.join(' ')
